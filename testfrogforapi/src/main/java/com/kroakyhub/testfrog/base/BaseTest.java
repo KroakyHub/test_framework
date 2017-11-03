a83d995412ca69f2fe96392f9001a367f1eab36d
Kroaky Toad <31759564+KroakyToad@users.noreply.github.com>
2017-11-03 12:45:17 +0530

    update from nishit

--- testfrogforapi/src/main/java/com/kroakyhub/testfrog/base/BaseTest.java
+++ testfrogforapi/src/main/java/com/kroakyhub/testfrog/base/BaseTest.java
@@ -4,98 +4,106 @@ import static io.restassured.RestAssured.given;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import org.apache.log4j.PropertyConfigurator;
+
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-public class BaseTest {
 
-	
-	public String frameworkClassPath = System.getenv("DRIVER_HOME");
-	public String testClassPath = System.getProperty("user.dir");
-	public static ExtentReports report;
-	public static ExtentTest test;
-	public String baseURI;
-	public String basePath;
-	public String endPoint;
-	public Map<String, Object> headerMap = new HashMap<String, Object>();
-	public Map<String, Object> queryParamMap = new HashMap<String, Object>();
-	public Map<String, Object> pathParamMap = new HashMap<String, Object>();
-	public Object pojo;
-	public String jsonFilePath;
 
-	public void initializeReports() {
-		String reportPath = testClassPath + "\\testfrogreport.html";
-		report = new ExtentReports(reportPath);
+public class BaseTest
+{
 
-		String log4jConfPath = frameworkClassPath + "\\log4j.properties";
-		PropertyConfigurator.configure(log4jConfPath);
-	}
 
-	public Response get() {
-		RestAssured.baseURI = baseURI;
-		RestAssured.basePath = basePath;
-		Response response = given().headers(headerMap).queryParams(queryParamMap).pathParams(pathParamMap).when()
-				.get(endPoint);
-		return response;
+    public String testClassPath = System.getProperty( "user.dir" );
+    public static ExtentReports report;
+    public static ExtentTest test;
+    public String baseURI;
+    public String basePath;
+    public String endPoint;
+    public Map<String, Object> headerMap = new HashMap<String, Object>();
+    public Map<String, Object> queryParamMap = new HashMap<String, Object>();
+    public Map<String, Object> pathParamMap = new HashMap<String, Object>();
+    public Object pojo;
+    public String jsonFilePath;
 
-	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Response post(Class c) {
+    public void initializeReports()
+    {
+        String reportPath = testClassPath + File.pathSeparator + "testfrogreport.html";
+        report = new ExtentReports( reportPath );
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		Response response = null;
-		try {
+        /*String log4jConfPath = frameworkClassPath + File.pathSeparator + "log4j.properties";
+        PropertyConfigurator.configure( log4jConfPath );*/
+    }
 
-			pojo = objectMapper.readValue(new File(testClassPath + jsonFilePath), c);
-			RestAssured.baseURI = baseURI;
-			RestAssured.basePath = basePath;
-			response = given().headers(headerMap).queryParams(queryParamMap).pathParams(pathParamMap).body(pojo).when()
-					.post(endPoint);
 
-		} catch (JsonParseException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public Response get()
+    {
+        RestAssured.baseURI = baseURI;
+        RestAssured.basePath = basePath;
+        Response response = given().headers( headerMap ).queryParams( queryParamMap ).pathParams( pathParamMap ).when()
+            .get( endPoint );
+        return response;
 
-		return response;
-	}
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public Response patch(Class c) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Response response = null;
-		try {
 
-			pojo = objectMapper.readValue(new File(testClassPath + jsonFilePath), c);
-			RestAssured.baseURI = baseURI;
-			RestAssured.basePath = basePath;
-			response = given().headers(headerMap).queryParams(queryParamMap).pathParams(pathParamMap).body(pojo).when()
-					.patch(endPoint);
+    @SuppressWarnings ( { "unchecked", "rawtypes" })
+    public Response post( Class c )
+    {
 
-		} catch (JsonParseException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        ObjectMapper objectMapper = new ObjectMapper();
+        Response response = null;
+        try {
 
-		return response;
-	}
+            pojo = objectMapper.readValue( new File( testClassPath + jsonFilePath ), c );
+            RestAssured.baseURI = baseURI;
+            RestAssured.basePath = basePath;
+            response = given().headers( headerMap ).queryParams( queryParamMap ).pathParams( pathParamMap ).body( pojo ).when()
+                .post( endPoint );
+
+        } catch ( JsonParseException e ) {
+            e.printStackTrace();
+        } catch ( JsonMappingException e ) {
+            e.printStackTrace();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+
+    @SuppressWarnings ( { "unchecked", "rawtypes" })
+    public Response patch( Class c )
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Response response = null;
+        try {
+
+            pojo = objectMapper.readValue( new File( testClassPath + jsonFilePath ), c );
+            RestAssured.baseURI = baseURI;
+            RestAssured.basePath = basePath;
+            response = given().headers( headerMap ).queryParams( queryParamMap ).pathParams( pathParamMap ).body( pojo ).when()
+                .patch( endPoint );
+
+        } catch ( JsonParseException e ) {
+            e.printStackTrace();
+        } catch ( JsonMappingException e ) {
+            e.printStackTrace();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
 
 }
